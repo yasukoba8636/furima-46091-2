df5062432cd12dc40cb3038e2239d6bd57d453dd
yasukoba8636 <yasukobatenri@gmail.com>
2025-10-29 22:52:15 +0900

    render repair

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,6 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
+
+# 初回デプロイ時にDBがない場合、自動で作成＆マイグレーション
+bundle exec rake db:prepare
