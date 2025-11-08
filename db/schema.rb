a656ed2bc911b83bc0e2197e1041c239902eeafe
yasukoba8636 <yasukobatenri@gmail.com>
2025-11-08 19:04:51 +0900

    activehash導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_08_085025) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_08_094929) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,14 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_08_085025) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "categories", charset: "utf8mb3", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "text", null: false
+    t.integer "categoory_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "items", charset: "utf8mb3", force: :cascade do |t|
